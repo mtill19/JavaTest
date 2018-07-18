b904e6c692b65d45028325f505a1662fe40e8d11
01545571 <01545571@LABW13405.ncc.commnet.edu>
2018-07-18 13:16:30 -0400

    second commit

--- JavaGitTest/src/javagittest/JavaGitTest.java
+++ JavaGitTest/src/javagittest/JavaGitTest.java
@@ -5,6 +5,11 @@ public class JavaGitTest {
     public static void main(String[] args) {
         System.out.println("Testing from NetBeans");
         System.out.println("Testing from GitHub");
+        
+        int x = 50;
+        int y = 2;
+        int product = x*y;
+        System.out.println(product);
     }
     
 }
