b2817ead298306a66ab9df748a6382964a70e319
mtill19 <41384613+mtill19@users.noreply.github.com>
2018-07-18 13:13:01 -0400

    Update JavaGitTest.java

--- JavaGitTest/src/javagittest/JavaGitTest.java
+++ JavaGitTest/src/javagittest/JavaGitTest.java
@@ -4,6 +4,7 @@ public class JavaGitTest {
 
     public static void main(String[] args) {
         System.out.println("Testing from NetBeans");
+        System.out.println("Testing from GitHub");
     }
     
 }
